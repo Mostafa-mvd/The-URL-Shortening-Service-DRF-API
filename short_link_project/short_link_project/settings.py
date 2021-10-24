40c93068e2734770e64a3398e16d10c41d923cca
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2021-10-24 15:49:53 +0330

    initialed local and 3rd party app

--- short_link_project/short_link_project/settings.py
+++ short_link_project/short_link_project/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd party
+    'rest_framework',
+
+    #local
+    'short_link_app.apps.ShortLinkAppConfig',
 ]
 
 MIDDLEWARE = [
