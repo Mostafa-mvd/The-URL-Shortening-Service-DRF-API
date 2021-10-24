43ece94baf103001cd9c1edd91a09757cf0338d3
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2021-10-24 19:47:34 +0330

    added ShortLinkCreator model and changed timezone in settings

--- short_link_project/short_link_app/models.py
+++ short_link_project/short_link_app/models.py
@@ -1,3 +1,44 @@
+from django.core import validators
 from django.db import models
+from . import utils as shortener_links_utils
 
-# Create your models here.
+
+class ShortLinkCreator(models.Model):
+
+    original_url = models.URLField(
+        validators=[
+            validators.URLValidator([
+                'http', 
+                'https',
+            ]),
+        ]
+    )
+
+    shorten_url = models.CharField(
+        unique=True,
+        max_length=18,
+        blank=True
+    )
+
+    created_time = models.DateTimeField(
+        auto_now_add=True
+    )
+    
+    expiration_time = models.DateTimeField(
+        default=shortener_links_utils.expire_at,
+        validators=[
+            shortener_links_utils.validate_expire_time,
+        ]
+    )
+
+    redirected_times = models.PositiveIntegerField(
+        default=0
+    )
+
+    is_shorten_url_private = models.BooleanField(
+        default=False,
+        verbose_name="Private URL?"
+    )
+
+    def __str__(self):
+        return "{} -> {}".format(self.original_url, self.shorten_url)
