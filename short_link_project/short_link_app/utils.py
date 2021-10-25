64d48764d761ce836c5b7c70f4c46c469f9a0b84
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2021-10-25 15:30:39 +0330

    added new func tools

--- short_link_project/short_link_app/utils.py
+++ short_link_project/short_link_app/utils.py
@@ -1,6 +1,10 @@
+from random import randint, choices
 from django.utils.timezone import now
 from datetime import timedelta
 from django.core.exceptions import ValidationError
+import string
+from datetime import datetime
+from . import models as shorten_link_model
 
 
 def expire_at():
@@ -15,3 +19,23 @@ def validate_expire_time(expiration_time):
 
     if expiration_time < now_time:
         raise ValidationError("Expiration time must be bigger than now")
+
+
+def create_shorten_link():
+    random_int = randint(8, 18)
+    random_shorten_link = ''.join(choices(
+        string.ascii_lowercase + string.digits, 
+        k=random_int))
+    
+    shorten_link_objects = shorten_link_model.ShortLinkCreator.objects
+    
+    if shorten_link_objects.filter(shorten_link=random_shorten_link).exists():
+        return create_shorten_link()
+    
+    return random_shorten_link
+
+
+def format_string_to_readable_datetime(datetime_str):
+    datetime_obj = datetime.fromisoformat(datetime_str)
+    formated_datetime = datetime_obj.strftime("%Y-%m-%d , %H:%M:%S")
+    return formated_datetime
