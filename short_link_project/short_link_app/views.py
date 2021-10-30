01657dba37c8b6a077c68f79817a1e2464379ffd
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2021-10-30 22:24:36 +0330

    added cache_page with redis cache memmory

--- short_link_project/short_link_app/views.py
+++ short_link_project/short_link_app/views.py
@@ -3,13 +3,15 @@ from rest_framework.response import Response
 from rest_framework.decorators import renderer_classes, api_view
 from rest_framework.renderers import JSONRenderer
 
-from users import utils as users_utils
-
 from django.shortcuts import get_object_or_404, redirect
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
 
 from . import (utils as shortening_link_utils,
                 serializers as shortening_link_serializers, 
                models as shortening_Link_models)
+
+from users import utils as users_utils
 
 
 class CreateShortenLink(generics.CreateAPIView):
@@ -29,18 +31,10 @@ class CreateShortenLink(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         if (request.user.is_anonymous and
-            request.POST.get("is_shorten_url_private", None)):
-
-            login_url = shortening_link_utils.concatenate_domain_with_path(
-                self.request, "users_urls:login")
-
-            register_url = shortening_link_utils.concatenate_domain_with_path(
-                self.request, "users_urls:register")
+            request.POST.get("is_private", None)):
 
             return Response(data={
                 "authentication": "You are not authenticated for using private url",
-                "login_url": login_url,
-                "register_url": register_url
                 },
                 status=status.HTTP_403_FORBIDDEN
             )
@@ -50,9 +44,10 @@ class CreateShortenLink(generics.CreateAPIView):
         return self.represent_result(response_data)
 
 
+@cache_page(shortening_link_utils.get_cache_ttl())
 @api_view(('GET',))
 @renderer_classes((JSONRenderer,))
-def redirect_to_original_url(request, token, otp_code=None):
+def redirector(request, token, otp_code=None):
 
     short_link = get_object_or_404(
         klass=shortening_Link_models.ShortLink,
@@ -60,28 +55,19 @@ def redirect_to_original_url(request, token, otp_code=None):
 
     if short_link.is_private:
         if not request.user.is_authenticated:
-
-            login_url = shortening_link_utils.concatenate_domain_with_path(
-                request, "users_urls:login")
-
-            return Response(data={
+            return JsonResponse(data={
                 "OTP ERROR": "Your url is private , you have to login first",
-                "login_url": login_url
-                }, content_type="application/json")
+                })
 
         if otp_code:
             if not users_utils.is_totp_code_valid(otp_code, request.user):
-                return Response(data={
-                    "OTP ERROR": "otp code is not valid, after 120 seconds try again"
-                    }, content_type="application/json")
+                return JsonResponse(data={
+                    "OTP ERROR": "otp code is not valid"
+                    })
         else:
-            otp_generator_url = shortening_link_utils.concatenate_domain_with_path(
-                request, "users_urls:generate_otp_code")
-
-            return Response(data={
-                "OTP ERROR": "Your url is private , set access otp code first",
-                "otp_generator": otp_generator_url
-                }, content_type="application/json")
+            return JsonResponse(data={
+                "OTP ERROR": "Your url is private , set access otp code first"
+                })
 
     short_link.add_to_times_of_redirected_field()
 
