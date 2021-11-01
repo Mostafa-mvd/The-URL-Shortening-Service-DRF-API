abd9bbd550bace56c0b311f02be7018a2fc2789a
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2021-11-02 00:07:27 +0330

    changed and deleted some utils function here

--- short_link_project/users/utils.py
+++ short_link_project/users/utils.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.conf import settings
 from users import models as users_models
 import pyotp
@@ -8,46 +7,25 @@ def get_otp_code_ttl():
     """get otp code time to live from django settings,
         result is in seconds"""
 
-    OTP_DEFAULT_TIMEOUT = 60 * 5
+    OTP_DEFAULT_TIMEOUT = 60 * 3
     OPT_CODE_TTL = getattr(settings, 'OTP_TTL', OTP_DEFAULT_TIMEOUT)
     return OPT_CODE_TTL
 
-def generate_user_secret_key(user_id):
 
-    user_obj = get_user_model().objects.get(id=user_id)
+def is_totp_code_valid(otp_code):
+    otp_qs = users_models.UserOTPCode.objects.filter(otp_code=otp_code)
 
-    secret_key_str = pyotp.random_base32()
+    if otp_qs.exists():
+        otp_obj = otp_qs.first()
+        totp = pyotp.TOTP(
+            s=otp_obj.secret_key,
+            interval=get_otp_code_ttl())
 
-    if users_models.UserSecretKey.objects.filter(user_secret_key=secret_key_str).exists():
-        return generate_user_secret_key(user_id)
+        is_valid = totp.verify(otp_code)
+        otp_obj.delete()
 
-    secret_key_obj = users_models.UserSecretKey.objects.create(
-        user=user_obj,
-        user_secret_key=secret_key_str)
-
-    secret_key_obj.save()
-
-def generate_totp_code(user_obj):
-    secret_key_obj = users_models.UserSecretKey.objects.get(
-        user=user_obj)
-
-    totp = pyotp.TOTP(
-        s=secret_key_obj.user_secret_key, 
-        interval=get_otp_code_ttl())
-        
-    otp_code = totp.now()
-
-    return otp_code
-
-def is_totp_code_valid(totp_code, user_obj):
-    secret_key_obj = users_models.UserSecretKey.objects.get(user=user_obj)
-
-    totp = pyotp.TOTP(
-        s=secret_key_obj.user_secret_key, 
-        interval=get_otp_code_ttl())
-
-    is_valid = totp.verify(totp_code)
-
-    if not is_valid:
+        if is_valid:
+            return True
         return False
-    return True
+
+    return False
