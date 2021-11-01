8833156672d28d778b982093e749657005b9938a
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2021-11-02 00:07:54 +0330

    added GenerateOTPCode view

--- short_link_project/users/views.py
+++ short_link_project/users/views.py
@@ -1,39 +1,96 @@
-from rest_framework import status
+from rest_framework import status, views
+from rest_framework.response import Response
 
-from django.http.response import JsonResponse
+from django.contrib.auth import authenticate
 
-from dj_rest_auth.registration.views import RegisterView
-
-from users import utils as users_utils
-
-
-class UserRegistrationView(RegisterView):
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-
-        if (request.method == "POST" and
-                response.status_code == status.HTTP_201_CREATED):
-
-            registered_user_pk = response.data["user"]['pk']
-            users_utils.generate_user_secret_key(registered_user_pk)
-
-        return response
+from users import (utils as users_utils, 
+                   serializers as users_serializers,
+                   models as users_models)
     
+import pyotp
 
-def get_totp_code(request):
-    if request.user.is_authenticated:
 
-        totp_code = users_utils.generate_totp_code(request.user)
+class GenerateOTPCode(views.APIView):
+    serializer_class = users_serializers.UserOTPCodeSerializer
+    model = users_models.UserOTPCode
+    user_obj = None
+
+    def user_exists(self, username, password):
+        user = authenticate(username=username, password=password)
+
+        if user:
+            self.user_obj = user
+            return True
+
+        return False
     
-        return JsonResponse(
+    def get_user_obj(self):
+        return self.user_obj
+    
+    def get_otp_model(self):
+        return self.model
+    
+    def generate_random_totp_code_and_secret_key(self):
+        secret_key = pyotp.random_base32()
+        totp = pyotp.TOTP(s=secret_key, interval=users_utils.get_otp_code_ttl())
+        otp_code = totp.now()
+
+        return secret_key, otp_code
+
+    def perform_create_totp_code(self):
+        """this function generates and returns otp_obj,
+            if otp_obj existed, this would have update otp_obj"""
+
+        user_obj = self.get_user_obj()
+        otp_qs = self.get_otp_model().objects.filter(user=user_obj)
+        secret_key_str, otp_code_str_digit = self.generate_random_totp_code_and_secret_key()
+
+        if otp_qs.exists():
+            otp_obj = otp_qs.first()
+            otp_obj = otp_obj.update_then_return_obj(
+                secret_key_str, otp_code_str_digit)
+        else:
+            otp_obj = users_models.UserOTPCode(
+                user=user_obj,
+                secret_key=secret_key_str,
+                otp_code=int(otp_code_str_digit))
+            otp_obj.save()
+
+        return otp_obj
+
+    def create_otp_obj(self):
+        
+        totp_code_obj = self.perform_create_totp_code()
+
+        return Response(
             data={
-                "totp_code": totp_code,
+                "totp_code": str(totp_code_obj.otp_code),
                 "ttl in minutes": users_utils.get_otp_code_ttl() / 60
-            }
+            }, status=status.HTTP_201_CREATED
         )
+    
+    def get_serializer(self):
+        return self.serializer_class
 
-    return JsonResponse(
-        data={
-            "Authentication Error": "You didn't log in"
-        }
-    )
+    def post(self, request):
+        serializer = self.get_serializer()
+        serialized_data = serializer(data=request.data)
+
+        if serialized_data.is_valid():
+            username = serialized_data.validated_data['username']
+            password = serialized_data.validated_data['password']
+
+            if self.user_exists(username=username, password=password):
+                json_response = self.create_otp_obj()
+                return json_response
+
+            return Response(
+                data={
+                    "ERROR": "username and password didn't match"
+                }, status=status.HTTP_401_UNAUTHORIZED)
+
+        return Response(
+            data={
+                "ERROR": "Your data is not valid."
+            }, status=status.HTTP_400_BAD_REQUEST)
+
