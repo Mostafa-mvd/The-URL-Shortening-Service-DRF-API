fa1e4a0bbbb5d42783bfb3748859e88db2e80dfd
Mostafa Vahdani <mostafa9102vahdani@outlook.com>
2021-11-01 23:58:37 +0330

    replaced UserOTPCode model with UserSecretKey model

--- short_link_project/users/models.py
+++ short_link_project/users/models.py
@@ -1,15 +1,24 @@
+from re import T
 from django.db import models
 from django.contrib.auth import get_user_model
 
 
-class UserSecretKey(models.Model):
-
+class UserOTPCode(models.Model):
     user = models.OneToOneField(
         to=get_user_model(),
         on_delete=models.CASCADE
     )
 
-    user_secret_key = models.TextField()
+    secret_key = models.CharField(max_length=250, unique=True)
+
+    otp_code = models.IntegerField(unique=True)
 
     def __str__(self) -> str:
-        return self.user_secret_key
+        return f'{self.otp_code}'
+    
+    def update_then_return_obj(self, secret_key_value, otp_code_value):
+        self.otp_code = otp_code_value
+        self.secret_key = secret_key_value
+        self.save()
+        return self
+        
